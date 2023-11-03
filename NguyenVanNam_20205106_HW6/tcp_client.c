75a828d5b0efb16d0eedbe32f30072f11c86d760
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-11-03 07:00:47 +0000

    add

---
+++ NguyenVanNam_20205106_HW6/tcp_client.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#define ERROR "Error Invalid String"
+#define BUFF_SIZE 8192
+
+void printMenu()
+{
+    printf("MENU\n");
+    printf("--------------------------------------\n");
+    printf("1. Gui xau bat ky\n");
+    printf("2. Gui noi dung mot file\n");
+}
+
+void sendMessage(int client_sock, char *buff, int msg_len)
+{
+    int bytes_sent = send(client_sock, buff, msg_len, 0);
+    if (bytes_sent <= 0)
+    {
+        printf("\nConnection closed!\n");
+        return;
+    }
+}
+void receiveMessage(int client_sock, char *buff, int msg_len)
+{
+    int bytes_received = recv(client_sock, buff, msg_len, 0);
+    if (bytes_received <= 0)
+    {
+        printf("\nConnection closed!\n");
+        return;
+    }
+    buff[bytes_received] = '\0';
+}
+
+void sendFile(int client_sock, char *fileName)
+{
+    FILE *f = fopen(fileName, "r");
+    if (f == NULL)
+    {
+        printf("Error: File not found\n");
+        return;
+    }
+    // đọc file
+    char file_data[BUFF_SIZE];
+    size_t bytesRead;
+    while ((bytesRead = fread(file_data, sizeof(char), BUFF_SIZE, f)) > 0)
+    {
+        sendMessage(client_sock, file_data, bytesRead);
+        memset(file_data, 0, BUFF_SIZE);
+    }
+    sendMessage(client_sock, "E", strlen("E"));
+
+    fclose(f);
+}
+
+int main(int argc, char *argv[])
+{
+    int SERVER_PORT;
+    char *SERVER_ADDR;
+    if (argc != 3)
+    {
+        printf("Usage: %s <server address> <port>\n", argv[0]);
+        return 0;
+    }
+    SERVER_ADDR = argv[1];
+    SERVER_PORT = atoi(argv[2]);
+    int client_sock;
+    char buff[BUFF_SIZE];
+    struct sockaddr_in server_addr; /* server's address information */
+    int msg_len;
+
+    // Step 1: Construct socket
+    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Step 2: Specify server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    // Step 3: Request to connect server
+    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0)
+    {
+        printf("\nError!Can not connect to sever! Client exit imediately!\n");
+        exit(1);
+    }
+    int choise;
+    // Step 4: Communicate with server
+    while (1)
+    {
+        printMenu();
+        scanf("%d", &choise);
+        printf("Choise: %d\n", choise);
+        switch (choise)
+        {
+        case 1:
+            printf("Nhap xau ky tu: ");
+            memset(buff, 0, BUFF_SIZE);
+            int c;
+            sendMessage(client_sock, "S", strlen("S"));
+            while ((c = getchar()) != '\n' && c != EOF)
+            {
+            }
+            fgets(buff, BUFF_SIZE, stdin);
+            buff[strlen(buff) - 1] = '\0';
+            msg_len = strlen(buff);
+
+            if (msg_len == 1)
+            {
+                printf("Bye\n");
+                exit(0);
+            }
+            sendMessage(client_sock, buff, msg_len);
+            receiveMessage(client_sock, buff, BUFF_SIZE);
+            if (strcmp(buff, ERROR) == 0)
+            {
+                printf("Error: Invalid String\n");
+                break;
+            }
+            printf("Repply from server: %s\n", buff);
+            memset(buff, 0, BUFF_SIZE);
+            break;
+        case 2:
+            printf("Nhap ten file: ");
+            memset(buff, 0, BUFF_SIZE);
+            sendMessage(client_sock, "F", strlen("F"));
+            while ((c = getchar()) != '\n' && c != EOF)
+            {
+            }
+            fgets(buff, BUFF_SIZE, stdin);
+            buff[strlen(buff) - 1] = '\0';
+            sendFile(client_sock, buff);
+            break;
+
+        default:
+            printf("Invalid choise\n");
+            break;
+        }
+
+        // receive echo reply
+    }
+
+    // Step 4: Close socket
+    close(client_sock);
+    return 0;
+}
---
+++ NguyenVanNam_20205106_HW6/tcp_server.c
@@ -0,0 +1,210 @@
+#include <stdio.h> /* These are the usual header files */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <openssl/md5.h>
+#define BACKLOG 2 /* Number of allowed connections */
+#define BUFF_SIZE 8192
+#define STRING_IN "S"
+#define FILE_IN "F"
+#define ERROR "Error Invalid String"
+void receiveMessage(int client_sock, char *buff, int msg_len)
+{
+    int bytes_received = recv(client_sock, buff, msg_len, 0);
+    if (bytes_received <= 0)
+    {
+        perror("\nError: ");
+        return;
+    }
+    buff[bytes_received] = '\0';
+}
+void sendMessage(int client_sock, char *buff, int msg_len)
+{
+    int bytes_sent = send(client_sock, buff, msg_len, 0);
+    if (bytes_sent <= 0)
+    {
+        perror("\nError: ");
+
+        return;
+    }
+}
+
+char *md5Hashing(char *str)
+{
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    MD5_CTX md5;
+    MD5_Init(&md5);
+    MD5_Update(&md5, str, strlen(str));
+    MD5_Final(digest, &md5);
+    char *md5Hash = (char *)malloc(33);
+    for (int i = 0; i < 16; i++)
+        sprintf(&md5Hash[i * 2], "%02x", (unsigned int)digest[i]);
+    return md5Hash;
+}
+char *digitInMD5(char *str)
+{
+    char *digit = (char *)malloc(BUFF_SIZE);
+    int j = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            digit[j] = str[i];
+            j++;
+        }
+    }
+    digit[j] = '\0';
+    return digit;
+}
+
+char *letterInMD5(char *str)
+{
+    char *letter = (char *)malloc(BUFF_SIZE);
+    int j = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            letter[j] = str[i];
+            j++;
+        }
+    }
+    letter[j] = '\0';
+    return letter;
+}
+int isValidString(char *str)
+{
+    int i = 0;
+    while (i < strlen(str))
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') ||
+            (str[i] >= '0' && str[i] <= '9') || str[i] == ' ')
+        {
+            i++;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 0;
+    }
+    int PORT = atoi(argv[1]);
+    int listen_sock, conn_sock; /* file descriptors */
+    char recv_data[BUFF_SIZE];
+    struct sockaddr_in server; /* server's address information */
+    struct sockaddr_in client; /* client's address information */
+
+    // Step 1: Construct a TCP socket to listen connection request
+    if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    { /* calls socket() */
+        perror("\nError: ");
+        return 0;
+    }
+    socklen_t sin_size;
+    // Step 2: Bind address to socket
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);              /* Remember htons() from "Conversions" section? =) */
+    server.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY puts your IP address automatically */
+    if (bind(listen_sock, (struct sockaddr *)&server, sizeof(server)) == -1)
+    { /* calls bind() */
+        perror("\nError: ");
+        return 0;
+    }
+
+    // Step 3: Listen request from client
+    if (listen(listen_sock, BACKLOG) == -1)
+    { /* calls listen() */
+        perror("\nError: ");
+        return 0;
+    }
+
+    // Step 4: Communicate with client
+    while (1)
+    {
+        // accept request
+        sin_size = sizeof(struct sockaddr_in);
+        if(conn_sock = accept(listen_sock, (struct sockaddr *)&client, &sin_size) < 0){
+            perror("\nError: ");
+            printf("Can't accept connection\n");
+            return 0;
+        }
+
+        printf("You got a connection from %s\n", inet_ntoa(client.sin_addr)); /* prints client's IP */
+
+        // start conversation
+        printf("Listening...\n");
+        while (1)
+        {
+            // receives message from client
+            receiveMessage(conn_sock, recv_data, BUFF_SIZE);
+            printf("Receive: %s\n", recv_data);
+            if (strcmp(recv_data, "S") == 0)
+            {
+                receiveMessage(conn_sock, recv_data, BUFF_SIZE);
+                printf("Receive string: %s\n", recv_data);
+                // check if recv_data is contain a number or letter, if not, send error message
+                if (!isValidString(recv_data))
+                {
+                    sendMessage(conn_sock, ERROR, strlen(ERROR));
+                }
+                else
+                {
+                    char *md5Hash = md5Hashing(recv_data);
+                    char *digit = digitInMD5(md5Hash);
+                    char *letter = letterInMD5(md5Hash);
+                    char *result = (char *)malloc(BUFF_SIZE);
+                    strcpy(result, digit);
+                    strcat(result, " ");
+                    strcat(result, letter);
+                    sendMessage(conn_sock, result, strlen(result));
+                }
+            }
+            else if (strcmp(recv_data, "F") == 0)
+            {
+                // nhận nội dung và in nội dung ra màn hình đến khi nhận được "E"
+                FILE *f = fopen("receive.png", "wb");
+                if (f == NULL)
+                {
+                    printf("Error: Can't open file\n");
+                    return 0;
+                }
+
+                size_t totalBytesReceived = 0;
+                while (1)
+                {
+                    receiveMessage(conn_sock, recv_data, BUFF_SIZE);
+                    // Write received data to the file
+                    if (strstr(recv_data, "E") != NULL)
+                    {
+                        break;
+                    }
+                    size_t bytesToWrite = strlen(recv_data);
+                    fwrite(recv_data, 1, bytesToWrite, f);
+                    totalBytesReceived += bytesToWrite;
+                    memset(recv_data, 0, BUFF_SIZE);
+                }
+
+                fclose(f);
+                printf("File received and saved as receive.png\n");
+
+            } // end conversation
+            close(conn_sock);
+        }
+
+        close(listen_sock);
+        return 0;
+    }
+}
